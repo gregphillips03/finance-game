f6d8d977a5c09e3e8440d202d041bc7024290a9a
Greg Phillips <greg.phillips@aecom.com>
2018-02-20 17:48:29 -0500

    added in the picture for the 404 page

--- finance-game-app/src/containers/NotFound.js
+++ finance-game-app/src/containers/NotFound.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./NotFound.css";
+import Robot from "../images/robot-msg-error.png"; 
 
 export default () =>
   <div className="NotFound">
     <h3>Sorry, page not found!</h3>
+    <img src={Robot} className="four-oh-four-error" alt="404 Error" />
   </div>;
