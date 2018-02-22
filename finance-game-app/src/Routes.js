06c16a39c4a82e5ca27c55fa8c2cf27765c7a5c3
Greg Phillips <greg.phillips@aecom.com>
2018-02-21 22:38:22 -0500

    created skeleton for leaderboard component and css

--- finance-game-app/src/Routes.js
+++ finance-game-app/src/Routes.js
@@ -6,6 +6,7 @@ import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
 import Signup from "./containers/Signup";
 import Dashboard from "./containers/Dashboard"; 
+import Leaderboard from "./containers/Leaderboard";
 
 export default ({ childProps }) =>
   <Switch>
@@ -13,6 +14,7 @@ export default ({ childProps }) =>
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <AppliedRoute path="/dashboard" exact component={Dashboard} props={childProps} />
+    <AppliedRoute path="/leaderboard" exact component={Leaderboard} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
---
+++ finance-game-app/src/containers/Leaderboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Leaderboard.css";
+
+export default class Leaderboard extends Component {
+  render() {
+    return (
+      <div className="Leaderboard">
+        <div className="lander">
+          <h1>Leaderboard</h1>
+          <p>This is the Leaderboard component being rendered to the DOM.</p>
+          <p>The Leaderboard will provide a graphical representation of the user's progress gauged against other players.</p>
+          <p>It will show a ordered list of avatars which rank each game player.</p>
+        </div>
+      </div>
+    );
+  }
+}
