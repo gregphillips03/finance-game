b6ec2f5c5eb4004f15db02f4ac4602b9e8eaaa78
Greg Phillips <greg.phillips@aecom.com>
2018-02-23 00:40:19 -0500

    prettying up the faction page:

--- finance-game-app/src/containers/Faction.js
+++ finance-game-app/src/containers/Faction.js
@@ -3,33 +3,34 @@ import "./Faction.css";
 import { Label } from "react-bootstrap"; 
 import Resist from "../images/resistance-logo.png";
 import Empire from "../images/empire-logo.png";
+import {Button} from "react-bootstrap"; 
 
 export default class Faction extends Component {
   render() {
     return (
       <div className="Faction">
         <div className="lander">
-          <h1>Faction</h1>
-          <p>This is the Faction component being rendered to the DOM.</p>
-          <p>The Faction page will allow the user to pick which side they want to <Label bsStyle="success">fight</Label> with during their current session.</p>
-          <p>This is where the user will land after they login.</p>
+          <h1>Choose A Faction</h1>
+          <p>Choose a faction to <Label bsStyle="danger">fight</Label> for.
+          You can change your faction each time you log in.</p>
+          <p>Be sure to check the <Label>Leadboard</Label> to see which faction is winning.</p>
         </div>
 
         <div className="row">
 
-          <div className="column">
+          <div className="column" id="column-click">
           <div className="card-resistance">
             <img src={Resist} alt="resistance-logo"/>
-            <h2>Resistance</h2>
-            <p>Join the Resistance</p>
+            <h2>Rebel Alliance</h2>
+          <Button bsSize="large" block><p>Join the Resistance</p></Button>
           </div>
           </div>
 
-          <div className="column">
+          <div className="column" id="column-click">
           <div className="card-empire">
             <img src={Empire} alt="empire-logo"/>
-            <h2>Empire</h2>
-            <p>Join the Dark Side</p>
+            <h2>Galactic Empire</h2>
+            <Button bsSize="large" block><p>Join the Dark Side</p></Button>
           </div>
           </div>
 
