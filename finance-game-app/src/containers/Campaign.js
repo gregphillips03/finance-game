db4ce131d09b44839da8658d7b6185fac99b7ead
Greg Phillips <greg.phillips@aecom.com>
2018-02-27 00:16:30 -0500

    done putting together the planets on the campaign page

--- finance-game-app/src/containers/Campaign.js
+++ finance-game-app/src/containers/Campaign.js
@@ -5,6 +5,19 @@ import Endor from "../images/planets/planet-endor.png";
 import Voss from "../images/planets/planet-voss.png";
 import Hoth from "../images/planets/planet-hoth.png";
 import Tatooine from "../images/planets/planet-tatooine.png";
+import Kamino from "../images/planets/planet-kamino.png";
+import NalHutta from "../images/planets/planet-nal-hutta.png";
+import Korriban from "../images/planets/planet-korriban.png";
+import Belsavis from "../images/planets/planet-belsavis.png";
+import Coruscant from "../images/planets/planet-coruscant.png";
+import Quesh from "../images/planets/planet-quesh.png";
+import Sabarene from "../images/planets/planet-sabarene.png";
+import Foron from "../images/planets/planet-foron.png";
+
+import NarShaddaa from "../images/planets/planet-nar-shaddaa.png";
+import Phorskuhii from "../images/planets/planet-phorskuh-ii.png";
+import DeathStar from "../images/planets/planet-death-star.png";
+import DeathStarii from "../images/planets/planet-death-star-ii.png";
 
 export default class Campaign extends Component {
     constructor(props) {
@@ -61,6 +74,114 @@ handleClick(campaignLocation){
             <img src={Tatooine} alt="planet"/>
             <h2>Tatooine</h2>
             <p>Anakin's Stomping Ground</p>
+          </div>
+          </div>
+
+        </div>
+
+        <div className="row">
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('kamino')}>
+          <div className="card-planet">
+            <img src={Kamino} alt="planet"/>
+            <h2>Kamino</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('voss')}>
+          <div className="card-planet">
+            <img src={NalHutta} alt="planet"/>
+            <h2>Nal Hutta</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('korriban')}>
+          <div className="card-planet">
+            <img src={Korriban} alt="planet"/>
+            <h2>Korriban</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('belsavis')}>
+          <div className="card-planet">
+            <img src={Belsavis} alt="planet"/>
+            <h2>Belsavis</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+        </div>
+
+        <div className="row">
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('coruscant')}>
+          <div className="card-planet">
+            <img src={Coruscant} alt="planet"/>
+            <h2>Coruscant</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('quesh')}>
+          <div className="card-planet">
+            <img src={Quesh} alt="planet"/>
+            <h2>Quesh</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('sabarene')}>
+          <div className="card-planet">
+            <img src={Sabarene} alt="planet"/>
+            <h2>Sabarene</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('foron')}>
+          <div className="card-planet">
+            <img src={Foron} alt="planet"/>
+            <h2>Foron</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+        </div>
+
+        <div className="row">
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('narshaddaa')}>
+          <div className="card-planet">
+            <img src={NarShaddaa} alt="planet"/>
+            <h2>Nar Shaddaa</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('phorskuhii')}>
+          <div className="card-planet">
+            <img src={Phorskuhii} alt="planet"/>
+            <h2>Phorskuh II</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('deathstar')}>
+          <div className="card-planet">
+            <img src={DeathStar} alt="planet"/>
+            <h2>Death Star</h2>
+            <p>Need Description</p>
+          </div>
+          </div>
+
+          <div className="column" id="column-click" onClick={()=>this.handleClick('deathstarii')}>
+          <div className="card-planet">
+            <img src={DeathStarii} alt="planet"/>
+            <h2>Death Star II</h2>
+            <p>Need Description</p>
           </div>
           </div>
 
