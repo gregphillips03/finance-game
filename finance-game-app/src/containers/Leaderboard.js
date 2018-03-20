7b0859c3150a0449b06907e15a591618d652bd60
Greg Phillips <greg.phillips@aecom.com>
2018-03-20 18:28:35 -0400

    removed dependency on unneeded native element react-native-elements

--- finance-game-app/src/containers/Leaderboard.js
+++ finance-game-app/src/containers/Leaderboard.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./Leaderboard.css";
 import { Label } from "react-bootstrap"; 
-import LeaderBoard from "../components/LeaderBoard"; 
 
 export default class Leaderboard extends Component {
   render() {
@@ -11,8 +10,7 @@ export default class Leaderboard extends Component {
           <h1>Leaderboard</h1>
           <p>This is the Leaderboard component being rendered to the DOM.</p>
           <p>The Leaderboard will provide a graphical representation of the user's progress gauged against other players.</p>
-          <p>It will show a ordered list of avatars which <Label bsStyle="success">rank each game player</Label> and show how each faction is performing.</p>
-          <p>The switch board below will allow the user to toggle between views.</p>        
+          <p>It will show a ordered list of avatars which <Label bsStyle="success">rank each game player</Label> and show how each faction is performing.</p> 
         </div>
       </div>
     );
