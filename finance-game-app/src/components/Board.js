21cbd9b9eb272a754e46081946df0423f4ac92d5
Greg Phillips <greg.phillips@aecom.com>
2018-03-20 20:11:44 -0400

    leaderboard experimenting

---
+++ finance-game-app/src/components/Board.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+//import "./Board.css";
+import { Label } from "react-bootstrap"; 
+
+export default class Board extends Component {
+  render() {
+    return (
+      <div className="Board">
+
+      </div>
+    );
+  }
+}
---
+++ finance-game-app/src/data/user-data.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export const userdata = [
+  {
+    "username": "Joe Camel",
+    "img": "",
+    "alltime": 2240, 
+    "recent": 431, 
+    "lastUpdate": "2016-03-06T00:20:14.620Z"
+  },
+  {
+    "username": "Spider Man",
+    "img": "",
+    "alltime": 421, 
+    "recent": 389, 
+    "lastUpdate": "2016-03-05T16:15:36.014Z"
+  }, 
+  {
+    "username": "Bat Man",
+    "img": "",
+    "alltime": 742, 
+    "recent":371, 
+    "lastUpdate": "2016-03-05T16:17:06.401Z"
+  },
+  {
+    "username": "Poison Ivy",
+    "img": "",
+    "alltime": 9876, 
+    "recent": 1000, 
+    "lastUpdate": "2016-04-05T16:17:06.401Z"
+  },
+  {
+    "username": "Harley Quinn",
+    "img": "",
+    "alltime": 3502, 
+    "recent": 20, 
+    "lastUpdate": "2016-10-05T16:17:06.401Z"
+  },
+];
