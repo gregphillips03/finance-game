2ca3a27cec44b1cd712b6fe6454370d6bf3ed258
Greg Phillips <greg.phillips@aecom.com>
2018-03-20 23:03:42 -0400

    formatting of leaderboard

--- finance-game-app/src/components/Board.js
+++ finance-game-app/src/components/Board.js
@@ -2,10 +2,35 @@ import React, { Component } from "react";
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { userdata } from '../data/user-data';
 import { Label } from "react-bootstrap";
+import MaleFace1 from "../images/faces-male/A01.png"; 
+import MaleFace2 from "../images/faces-male/A02.png"; 
+import FemFace1 from "../images/faces-female/FA01.png";
+import FemFace2 from "../images/faces-female/FA02.png"; 
 import '../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
+var i = 0 ; 
 
 function imageFormatter(cell, row){
   return (<img style={{width:80}} src={cell} className="leaderAvatar" alt="Avatar"/>)
+}
+
+function makeDefaultImage(cell, row){
+	i = i + 1; 
+	if( i > 4){
+		i = 1;
+	}
+	if(i === 1){
+		return (<img style={{width:80}} src={MaleFace1} className="leaderAvatar" alt="Avatar"/>)
+	}
+	else if (i === 2){
+		return (<img style={{width:80}} src={MaleFace2} className="leaderAvatar" alt="Avatar"/>)
+	}
+	else if (i === 3){
+		return (<img style={{width:80}} src={FemFace1} className="leaderAvatar" alt="Avatar"/>)
+	}
+	else{
+		return (<img style={{width:80}} src={FemFace2} className="leaderAvatar" alt="Avatar"/>)
+	}
 }
 
 function integerFormatter(cell, row){
@@ -34,9 +59,9 @@ export default class Board extends Component {
       				  hover={ true }
       				  options={ tableOptions}
       				  pagination>
-      	  <TableHeaderColumn dataField='img' dataFormat={imageFormatter}>Avatar</TableHeaderColumn>
-      	  <TableHeaderColumn dataField='level' dataFormat={integerFormatter}>Level</TableHeaderColumn>
-          <TableHeaderColumn dataField='username' isKey={ true } dataFormat={userNameFormatter}>Username</TableHeaderColumn>
+      	  <TableHeaderColumn dataField='img' width='15%' dataFormat={makeDefaultImage}>Avatar</TableHeaderColumn>
+      	  <TableHeaderColumn dataField='level' width='10%' dataFormat={integerFormatter}>Level</TableHeaderColumn>
+          <TableHeaderColumn dataField='username' width='30%' isKey={ true } dataFormat={userNameFormatter}>Username</TableHeaderColumn>
           <TableHeaderColumn dataField='alltime'>All Time XP</TableHeaderColumn>
           <TableHeaderColumn dataField='recent'>Recent XP</TableHeaderColumn>
       </BootstrapTable>
--- finance-game-app/src/data/user-data.js
+++ finance-game-app/src/data/user-data.js
@@ -1,8 +1,7 @@
-import React from 'react';
 
 export const userdata = [
   {
-    "username": "Joe Camel",
+    "username": "joe.camel@mail.umw.edu",
     "img": "https://avatars0.githubusercontent.com/u/19273069?s=460&v=4",
     "alltime": 2240, 
     "recent": 431, 
@@ -10,7 +9,7 @@ export const userdata = [
     "level": 5
   },
   {
-    "username": "Spider Man",
+    "username": "spider.man@mail.umw.edu",
     "img": "../src/images/faces-male/A01.png",
     "alltime": 421, 
     "recent": 389, 
@@ -18,7 +17,7 @@ export const userdata = [
     "level": 2
   }, 
   {
-    "username": "Bat Man",
+    "username": "bat.man@mail.umw.edu",
     "img": "",
     "alltime": 742, 
     "recent":371, 
@@ -26,7 +25,7 @@ export const userdata = [
     "level": 3
   },
   {
-    "username": "Poison Ivy",
+    "username": "poison.ivy@mail.umw.edu",
     "img": "",
     "alltime": 9876, 
     "recent": 1000, 
@@ -34,7 +33,7 @@ export const userdata = [
     "level": 8
   },
   {
-    "username": "Harley Quinn",
+    "username": "harley.quinn@mail.umw.edu",
     "img": "",
     "alltime": 3502, 
     "recent": 20, 
@@ -42,7 +41,7 @@ export const userdata = [
     "level": 6
   },
   {
-    "username": "Dirk Diggler",
+    "username": "dirk.diggler@mail.umw.edu",
     "img": "",
     "alltime": 3590, 
     "recent": 40, 
@@ -50,7 +49,7 @@ export const userdata = [
     "level": 6
   },
   {
-    "username": "Jim Jones",
+    "username": "jim.jones@mail.umw.edu",
     "img": "https://avatars0.githubusercontent.com/u/19273069?s=460&v=4",
     "alltime": 3245, 
     "recent": 0, 
@@ -58,7 +57,7 @@ export const userdata = [
     "level": 5
   },
   {
-    "username": "Cornholio",
+    "username": "corn.holio@mail.umw.edu",
     "img": "",
     "alltime": 6544, 
     "recent": 378, 
@@ -66,7 +65,7 @@ export const userdata = [
     "level": 4
   }, 
   {
-    "username": "Madison Ivy",
+    "username": "madison.ivy@mail.umw.edu",
     "img": "",
     "alltime": 3567, 
     "recent": 1202, 
@@ -74,7 +73,7 @@ export const userdata = [
     "level": 3
   },
   {
-    "username": "Rachel Roxxx",
+    "username": "rachel.roxxx@mail.umw.edu",
     "img": "",
     "alltime": 12899, 
     "recent": 928, 
@@ -82,7 +81,7 @@ export const userdata = [
     "level": 8
   },
   {
-    "username": "Dr. Sheldon Cooper",
+    "username": "sheldon.cooper@mail.umw.edu",
     "img": "",
     "alltime": 7865, 
     "recent": 6578, 
@@ -90,7 +89,7 @@ export const userdata = [
     "level": 8
   },
   {
-    "username": "Samwise Gamgee",
+    "username": "samwise.gamgee@mail.umw.edu",
     "img": "",
     "alltime": 567, 
     "recent": 72, 
@@ -98,7 +97,7 @@ export const userdata = [
     "level": 2
   },
   {
-    "username": "Optimus Prime",
+    "username": "optimus.prime@mail.umw.edu",
     "img": "https://avatars0.githubusercontent.com/u/19273069?s=460&v=4",
     "alltime": 5600, 
     "recent": 523, 
@@ -106,7 +105,7 @@ export const userdata = [
     "level": 6
   },
   {
-    "username": "Carrie Fisher",
+    "username": "carrie.fisher@mail.umw.edu",
     "img": "",
     "alltime": 8745, 
     "recent": 0, 
@@ -114,7 +113,7 @@ export const userdata = [
     "level": 4
   }, 
   {
-    "username": "Cosmo Kramer",
+    "username": "cosmo.kramer@mail.umw.edu",
     "img": "",
     "alltime": 23, 
     "recent": 5, 
@@ -122,7 +121,7 @@ export const userdata = [
     "level": 1
   },
   {
-    "username": "Dwight Schrute",
+    "username": "dwight.schrute@mail.umw.edu",
     "img": "",
     "alltime": 22908, 
     "recent": 2987, 
@@ -130,7 +129,7 @@ export const userdata = [
     "level": 12
   },
   {
-    "username": "Hamburgler McBurgler",
+    "username": "hamburgler.mcburgler@mail.umw.edu",
     "img": "",
     "alltime": 2786, 
     "recent": 502, 
@@ -138,7 +137,7 @@ export const userdata = [
     "level": 3
   },
   {
-    "username": "Snoop Doggity Dog",
+    "username": "snoop.doggity.dog@mail.umw.edu",
     "img": "",
     "alltime": 45670, 
     "recent": 1278, 
@@ -146,7 +145,7 @@ export const userdata = [
     "level": 10
   },
   {
-    "username": "Bon Jovi",
+    "username": "bon.jovi@mail.umw.edu",
     "img": "",
     "alltime": 2908, 
     "recent": 2107, 
@@ -154,7 +153,7 @@ export const userdata = [
     "level": 7
   },
   {
-    "username": "Charlie Sheen",
+    "username": "charlie.sheen@mail.umw.edu",
     "img": "",
     "alltime": 6969, 
     "recent": 501, 
@@ -162,11 +161,19 @@ export const userdata = [
     "level": 5
   },
   {
-    "username": "Lexi Belle",
+    "username": "lexi.belle@mail.umw.edu",
     "img": "",
     "alltime": 46750, 
     "recent": 5678, 
     "lastUpdate": "2018-10-05T16:17:06.401Z", 
     "level": 13
+  },
+  {
+    "username": "faye.raegen@mail.umw.edu",
+    "img": "",
+    "alltime": 15750, 
+    "recent": 478, 
+    "lastUpdate": "2018-10-05T16:17:06.401Z", 
+    "level": 11
   },
 ];
