c9e7c4403feee466be39efa91884e8d174d7f20e
Greg Phillips <greg.phillips@aecom.com>
2018-03-30 02:30:49 -0400

    hooked up backend to database for the faction data

--- finance-game-backend/routes/faction-data.js
+++ finance-game-backend/routes/faction-data.js
@@ -1,47 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var pgp = require('pg-promise')(/*options*/);
+const { Pool, Client } = require('pg')
 
-var connection = {
-  host     : process.env.RDS_HOSTNAME,
-  port     : process.env.RDS_PORT,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  dbname   : process.env.RDS_DBNAME
-}
+const pool = new Pool({
+  user: 'masterkey',
+  host: 'finance-game-db.cm0naqqd1wbd.us-west-2.rds.amazonaws.com',
+  database: 'gregtest',
+  password: 'chickendog123',
+  port: 5432,
+})
 
 /* GET faction data listing. */
-router.get('/', function(req, res, next) {
-  var db = pgp(connection); // database instance;
-  console.log("AAAAAaaaagghahaahhhhhh!!!!"); 
-  db.any('SELECT factionname, alltime, recent, level FROM gregtest').then(item => {res.json(item)}).catch(error => {console.log(error)});  
-    res.json([
-    {
-      "factionname": "Jedi Order",
-      "alltime": 134671, 
-      "recent": 14567,
-      "level": 4, 
-    },
-    {
-      "factionname": "Galactic Empire",
-      "alltime": 54678,
-      "recent": 3240, 
-      "level": 3, 
-    },
-    {
-      "factionname": "Rebel Alliance",
-      "alltime": 127890,
-      "recent": 4500, 
-      "level": 1, 
-    },
-    {
-      "factionname": "Rogue",
-      "alltime": 127654, 
-      "recent": 780,
-      "level": 1, 
-    },
-  ]);
-  res.end(); 
+router.get('/', async function(req, res, next) {
+  var response = await pool.query('SELECT factionname, alltime, recent, level FROM faction_data_test'); 
+  var data = response.rows.map(function(item){return {factionname: item.factionname, 
+    alltime: item.alltime, 
+    recent: item.recent, 
+    level: item.level};});
+    console.log(data); 
+    res.json(data); 
+    pool.end()
 });
 
 module.exports = router;
