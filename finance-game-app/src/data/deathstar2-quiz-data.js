efa8e4cf27a9530169f08cf234646e6556e8cc62
trevor-martineau <trevorwmartineau@gmail.com>
2018-04-14 15:55:00 -0400

    added quiz data files for each planet

---
+++ finance-game-app/src/data/deathstar2-quiz-data.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+export const questions = [
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  },
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  },
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  },
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  },
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  },
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  },
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  },
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  },
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  },
+  {
+    "question": <span></span>,
+    "answers": [
+      <span></span>,
+      <span></span>,
+      <span></span>,
+      <span></span>
+    ],
+    "correct": ""
+  }
+  
+];
