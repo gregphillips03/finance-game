b9ab57550548e71447a74c91edbc538f1e2bfe13
trevor-martineau <trevorwmartineau@gmail.com>
2018-04-14 16:43:04 -0400

    added 'easy' questions to endor-quiz-data

--- finance-game-app/src/data/endor-quiz-data.js
+++ finance-game-app/src/data/endor-quiz-data.js
@@ -2,52 +2,59 @@ import React from 'react';
 
 export const questions = [
   {
-    "question": <span></span>,
+    "difficulty": <span>"Easy"</span>, 
+    "question": <span>Given an interest rate of zero percent, the future value of a lump sum invested today will always.</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>Remain constant, regardless of the investment time period</span>,
+      <span>Be equal to zero</span>,
+      <span>Increase if you increase the investment time period</span>,
+      <span>Be infinite in value</span>
     ],
     "correct": ""
   },
   {
-    "question": <span></span>,
+    "difficulty": <span>"Easy"</span>, 
+    "question": <span>Which of the following statements is TRUE?</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>Present values and discount rates have a negative relationship.</span>,
+      <span>Present values and discount rates have a positive relationship.</span>,
+      <span>Future values and discount rates have a negative relationship.</span>,
+      <span>Present values and time have a positive relationship.</span>
     ],
     "correct": ""
   },
   {
-    "question": <span></span>,
+    "difficulty": <span>"Easy"</span>,
+    "question": <span>You expect to receive an inheritance of $300,000 
+    in six years. Your current savings are earning an average return of 7%. 
+    When you discount the expected inheritance to a value today, you are finding the .</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>Present Value</span>,
+      <span>Simple Value</span>,
+      <span>Future Value</span>,
+      <span>Compound Value</span>
     ],
     "correct": ""
   },
   {
-    "question": <span></span>,
+    "difficulty": <span>"Easy"</span>,
+    "question": <span>PVF is an acronym used to denote the ______.</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>Present Value Factor</span>,
+      <span>Pure Value Factor</span>,
+      <span>Profit Value Factor</span>,
+      <span>Price Variation Function</span>
     ],
     "correct": ""
   },
   {
-    "question": <span></span>,
+    "difficulty": <span>"Easy"</span>,
+    "question": <span>PV is an acronym used to denote ______.</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>Present Value</span>,
+      <span>Past Value</span>,
+      <span>Profit Value</span>,
+      <span>Profit Vector</span>
     ],
     "correct": ""
   },
