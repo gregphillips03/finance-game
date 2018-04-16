aeddaf41b6488901094c8c75a75b91c5d8f2191e
trevor-martineau <trevorwmartineau@gmail.com>
2018-04-16 18:32:42 -0400

    added some technical questions to endor-quiz-data as well as added a difficulty to each question

--- finance-game-app/src/data/endor-quiz-data.js
+++ finance-game-app/src/data/endor-quiz-data.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const questions = [
   {
-    "difficulty": <span>"Easy"</span>, 
+    "difficulty": <span>Easy</span>, 
     "question": <span>Given an interest rate of zero percent, the future value of a lump sum invested today will always.</span>,
     "answers": [
       <span>Remain constant, regardless of the investment time period</span>,
@@ -10,10 +10,10 @@ export const questions = [
       <span>Increase if you increase the investment time period</span>,
       <span>Be infinite in value</span>
     ],
-    "correct": ""
+    "correct": "Remain constant, regardless of the investment time period"
   },
   {
-    "difficulty": <span>"Easy"</span>, 
+    "difficulty": <span>Easy</span>, 
     "question": <span>Which of the following statements is TRUE?</span>,
     "answers": [
       <span>Present values and discount rates have a negative relationship.</span>,
@@ -21,10 +21,10 @@ export const questions = [
       <span>Future values and discount rates have a negative relationship.</span>,
       <span>Present values and time have a positive relationship.</span>
     ],
-    "correct": ""
+    "correct": "Present values and discount rates have a negative relationship."
   },
   {
-    "difficulty": <span>"Easy"</span>,
+    "difficulty": <span>Easy</span>,
     "question": <span>You expect to receive an inheritance of $300,000 
     in six years. Your current savings are earning an average return of 7%. 
     When you discount the expected inheritance to a value today, you are finding the .</span>,
@@ -34,10 +34,10 @@ export const questions = [
       <span>Future Value</span>,
       <span>Compound Value</span>
     ],
-    "correct": ""
+    "correct": "Present Value"
   },
   {
-    "difficulty": <span>"Easy"</span>,
+    "difficulty": <span>Easy</span>,
     "question": <span>PVF is an acronym used to denote the ______.</span>,
     "answers": [
       <span>Present Value Factor</span>,
@@ -45,7 +45,7 @@ export const questions = [
       <span>Profit Value Factor</span>,
       <span>Price Variation Function</span>
     ],
-    "correct": ""
+    "correct": "Present Value Factor"
   },
   {
     "difficulty": <span>"Easy"</span>,
@@ -56,49 +56,61 @@ export const questions = [
       <span>Profit Value</span>,
       <span>Profit Vector</span>
     ],
-    "correct": ""
+    "correct": "Present Value"
   },
   {
-    "question": <span></span>,
+    "difficulty": <span>Medium</span>,
+    "question": <span>What is the present 
+    value of $6,000 to be received in 5 years 
+    if the discount rate is 12%, compounded monthly?</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>$3,302.70</span>,
+      <span>$5,708.79</span>,
+      <span>$3,404.56</span>,
+      <span>$5,205.44</span>
     ],
-    "correct": ""
+    "correct": "$3,302.70"
   },
   {
-    "question": <span></span>,
+    "difficulty":<span>Medium</span>,
+    "question": <span>What is the future value of $8,500 that
+     you have today if you invest it 
+     for 2 years at 9%, compounded quarterly?</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>$10,156.06</span>,
+      <span>$8,886.80</span>,
+      <span>$10,098.85</span>,
+      <span>$16,936.78</span>
     ],
-    "correct": ""
+    "correct": "$10,156.06"
   },
   {
-    "question": <span></span>,
+    "difficulty":<span>Medium</span>,
+    "question": <span>How long will it take to grow $3,000 today to
+     $8,000 if you invest it at 10%, compounded daily?</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>9.81 years</span>,
+      <span>Over 3,000 years.</span>,
+      <span>10.29 years.</span>,
+      <span>Impossible to determine.</span>
     ],
-    "correct": ""
+    "correct": "9.81 years"
   },
   {
-    "question": <span></span>,
+    "difficulty":<span>Medium</span>,
+    "question": <span>What annual return do you need to earn if you would 
+    like $125,000 to grow to $200,000 within four years and the account 
+    compounds semiannually?</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>12.47%</span>,
+      <span>6.05%</span>,
+      <span>12.10%</span>,
+      <span>Impossible to determine.</span>
     ],
-    "correct": ""
+    "correct": "12.47%"
   },
   {
+    "difficulty":<span></span>,
     "question": <span></span>,
     "answers": [
       <span></span>,
