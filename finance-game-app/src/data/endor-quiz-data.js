4c6519ac7f92c838e2dfdc8f8fcac37e865d8ece
trevor-martineau <trevorwmartineau@gmail.com>
2018-04-17 12:02:25 -0400

    finished adding technical/conceptual questions to endor-quiz-data.js

--- finance-game-app/src/data/endor-quiz-data.js
+++ finance-game-app/src/data/endor-quiz-data.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const questions = [
   {
-    "difficulty": <span>Easy</span>, 
+    "difficulty": <span>1</span>, 
     "question": <span>Given an interest rate of zero percent, the future value of a lump sum invested today will always.</span>,
     "answers": [
       <span>Remain constant, regardless of the investment time period</span>,
@@ -13,7 +13,7 @@ export const questions = [
     "correct": "Remain constant, regardless of the investment time period"
   },
   {
-    "difficulty": <span>Easy</span>, 
+    "difficulty": <span>1</span>, 
     "question": <span>Which of the following statements is TRUE?</span>,
     "answers": [
       <span>Present values and discount rates have a negative relationship.</span>,
@@ -24,7 +24,7 @@ export const questions = [
     "correct": "Present values and discount rates have a negative relationship."
   },
   {
-    "difficulty": <span>Easy</span>,
+    "difficulty": <span>1</span>,
     "question": <span>You expect to receive an inheritance of $300,000 
     in six years. Your current savings are earning an average return of 7%. 
     When you discount the expected inheritance to a value today, you are finding the .</span>,
@@ -37,7 +37,7 @@ export const questions = [
     "correct": "Present Value"
   },
   {
-    "difficulty": <span>Easy</span>,
+    "difficulty": <span>1</span>,
     "question": <span>PVF is an acronym used to denote the ______.</span>,
     "answers": [
       <span>Present Value Factor</span>,
@@ -48,7 +48,7 @@ export const questions = [
     "correct": "Present Value Factor"
   },
   {
-    "difficulty": <span>"Easy"</span>,
+    "difficulty": <span>1</span>,
     "question": <span>PV is an acronym used to denote ______.</span>,
     "answers": [
       <span>Present Value</span>,
@@ -59,7 +59,78 @@ export const questions = [
     "correct": "Present Value"
   },
   {
-    "difficulty": <span>Medium</span>,
+    "difficulty": <span>2</span>,
+    "question": <span>You deposited $2,400 two years ago into an account 
+    that earns a 6% return, compounded annually. Compounded annually means ____________.</span>,
+    "answers": [
+      <span>The rate of return compounds once per year.</span>,
+      <span>The rate of return is simple interest.</span>,
+      <span>The rate of return does not exist.</span>,
+      <span>The rate of return is in the past, so not relevant.</span>
+    ],
+    "correct": "The rate of return compounds once per year."
+  },
+  {
+    "difficulty": <span>2</span>,
+    "question": <span>You deposited $2,400 two years ago into an account that earns a 
+    6% return, compounded annually. Relative to two years ago, the amount you have 
+    today is a ____________.</span>,
+    "answers": [
+      <span>Future value</span>,
+      <span>Present value</span>,
+      <span>Compound value</span>,
+      <span>Simple Value</span>
+    ],
+    "correct": "Future value"
+  },
+  {
+    "difficulty": <span>2</span>,
+    "question": <span>You deposited $2,400 two years ago into an account that earns a 
+    6% return, compounded annually. Relative to today, the $2,400 is a ____________.</span>,
+    "answers": [
+      <span>Present value</span>,
+      <span>Future value</span>,
+      <span>Compound value</span>,
+      <span>Simple value</span>
+    ],
+    "correct": "Present value"
+  },
+  {
+    "difficulty": <span>2</span>,
+    "question": <span>You currently have $2,000 and access to an account 
+    that earns 15%, compounded quarterly. Compounded quarterly means ____________.</span>,
+    "answers": [
+      <span>One quarter of the 15% compounds each quarter.</span>,
+      <span>The 15% compounds once per year.</span>,
+      <span>One quarter of the 15% compounds each year.</span>,
+      <span>The 15% compounds each quarter</span>
+    ],
+    "correct": "One quarter of the 15% compounds each quarter."
+  },
+  {
+    "difficulty": <span>2</span>,
+    "question": <span>Which of the following has the highest value 5 years from today, all else equal?</span>,
+    "answers": [
+      <span>Future value with monthly compounding</span>,
+      <span>Future value with annual compounding</span>,
+      <span>Future value with semiannual compounding</span>,
+      <span>Future value with quarterly compounding</span>
+    ],
+    "correct": "Future value with monthly compounding"
+  },
+  {
+    "difficulty": <span>2</span>,
+    "question": <span>Which of the following has the highest value TODAY, all else equal?</span>,
+    "answers": [
+      <span>Present value with annual compounding</span>,
+      <span>Present value with semiannual compounding</span>,
+      <span>Present value with daily compounding</span>,
+      <span>Present value with monthly compounding</span>
+    ],
+    "correct": "Present value with annual compounding"
+  },
+  {
+    "difficulty": <span>2</span>,
     "question": <span>What is the present 
     value of $6,000 to be received in 5 years 
     if the discount rate is 12%, compounded monthly?</span>,
@@ -72,7 +143,7 @@ export const questions = [
     "correct": "$3,302.70"
   },
   {
-    "difficulty":<span>Medium</span>,
+    "difficulty":<span>2</span>,
     "question": <span>What is the future value of $8,500 that
      you have today if you invest it 
      for 2 years at 9%, compounded quarterly?</span>,
@@ -85,7 +156,7 @@ export const questions = [
     "correct": "$10,156.06"
   },
   {
-    "difficulty":<span>Medium</span>,
+    "difficulty":<span>2</span>,
     "question": <span>How long will it take to grow $3,000 today to
      $8,000 if you invest it at 10%, compounded daily?</span>,
     "answers": [
@@ -97,7 +168,7 @@ export const questions = [
     "correct": "9.81 years"
   },
   {
-    "difficulty":<span>Medium</span>,
+    "difficulty":<span>2</span>,
     "question": <span>What annual return do you need to earn if you would 
     like $125,000 to grow to $200,000 within four years and the account 
     compounds semiannually?</span>,
@@ -110,15 +181,148 @@ export const questions = [
     "correct": "12.47%"
   },
   {
-    "difficulty":<span></span>,
-    "question": <span></span>,
+    "difficulty":<span>3</span>,
+    "question": <span>You deposited $2,400 two years ago into an account that 
+    earns a 6% return, compounded annually. How much do you have in the account today?</span>,
     "answers": [
-      <span></span>,
-      <span></span>,
-      <span></span>,
-      <span></span>
+      <span>$2,696.64</span>,
+      <span>$2,135.99</span>,
+      <span>$2,400.00</span>,
+      <span>$2,778.30</span>
     ],
-    "correct": ""
+    "correct": "$2,696.64"
+  },
+  {
+    "difficulty":<span>3</span>,
+    "question":<span>You deposited $2,400 two years ago into an account that earns 
+    a 6% return, compounded annually. How much do you have in the account three 
+    years from today?</span>,
+    "answers": [
+      <span>$3,211.74</span>,
+      <span>$2,696.64</span>,
+      <span>$2,135.99</span>,
+      <span>$2,858.44</span>
+    ],
+    "correct": "$3,211.74"
+  },
+  {
+    "difficulty":<span>3</span>,
+    "question":<span>You expect to inherit $350,000 in 40 years. 
+    If you have access to an account that earns an 8% return, 
+    compounded monthly, how much is your inheritance worth today?</span>,
+    "answers": [
+      <span>$14,419.08</span>,
+      <span>$16,110.83</span>,
+      <span>$268,312.26</span>,
+      <span>$0.00</span>
+    ],
+    "correct": "$14,419.08"
+  },
+  {
+    "difficulty":<span>3</span>,
+    "question":<span>You expect to inherit $350,000 in 40 years. If you have access 
+    to an account that earns an 8% return, compounded monthly, 
+    how much is your inheritance worth ten years from today?</span>,
+    "answers": [
+      <span>$32,005.18</span>,
+      <span>$16,110.83</span>,
+      <span>$14,419.08</span>,
+      <span>$34,782.07</span>
+    ],
+    "correct": "$32,005.18"
+  },
+  {
+    "difficulty":<span>3</span>,
+    "question":<span>You opened an investment account 28 years ago and 
+    deposited $5,000 into the account. Today, the account is worth $28,000. 
+    What annual return did your account earn?</span>,
+    "answers": [
+      <span>6.35%</span>,
+      <span>10.25%</span>,
+      <span>2.25%</span>,
+      <span>Impossible to determine.</span>
+    ],
+    "correct": "6.35%"
+  },
+  {
+    "difficulty":<span>3</span>,
+    "question":<span>You currently have $4,000 and access to an account 
+    that earns a 10% return annually. You would like to leave the money in 
+    the account until it has tripled. How long will it take?</span>,
+    "answers": [
+      <span>11.5 years</span>,
+      <span>5 years</span>,
+      <span>10.5 years</span>,
+      <span>Impossible to determine</span>
+    ],
+    "correct": "11.5 years"
+  },
+  {
+    "difficulty":<span>3</span>,
+    "question":<span>How much money do you need to have today in an 
+    account that earns 8% annually if you’d like to have $100,000 in ten years?</span>,
+    "answers": [
+      <span>$46,319.35</span>,
+      <span>$10,156.24</span>,
+      <span>$25,005.56</span>,
+      <span>$65,562.33</span>
+    ],
+    "correct": "$46,319.35"
+  },
+  {
+    "difficulty":<span>3</span>,
+    "question":<span>You currently have $2,000 and access to an 
+    account that earns 15%, compounded quarterly. You need to have $6,000 for a 
+    down payment 3 years from today. How much do you need to add to 
+    the account today in order to have a total of $6,000 in 3 years?</span>,
+    "answers": [
+      <span>$1,857.39</span>,
+      <span>$3,857.39</span>,
+      <span>$1,945.10</span>,
+      <span>$3,945.10</span>
+    ],
+    "correct": "$1,857.39"
+  },
+  {
+    "difficulty":<span>4</span>,
+    "question":<span>You have $20,000 to invest and have been offered an 
+    investment plan that pays 7% per year for the next 20 years. 
+    After that, the investment plan pays 11% per year. If you invest in this plan, 
+    how much will you have after 35 years?</span>,
+    "answers": [
+      <span>$370,297.03</span>,
+      <span>$77,393.69</span>,
+      <span>$771,497.02</span>,
+      <span>$213,531.63</span>
+    ],
+    "correct": "$370,297.03"
+  },
+  {
+    "difficulty":<span>4</span>,
+    "question":<span>You expect to inherit $200,000 in 20 years and have access 
+    to an account that will earn 5% per year for the next 7 years and 8% per 
+    year after that. The account compounds semiannually. What is the value of your 
+    inheritance today?</span>,
+    "answers": [
+      <span>$51,053.92</span>,
+      <span>$73,539.58</span>,
+      <span>$42,909.64</span>,
+      <span>$56,759.41</span>
+    ],
+    "correct": "$51,053.92"
+  },
+  {
+    "difficulty":<span>4</span>,
+    "question":<span>You expect to inherit $200,000 in 20 years and have access to 
+    an account that will earn 5% per year for the next 7 years and 8% per year 
+    after that. The account compounds semiannually. What is the value of your 
+    inheritance 5 years from today?</span>,
+    "answers": [
+      <span>$65,353.33</span>,
+      <span>$73,539.58</span>,
+      <span>$52,263.21</span>,
+      <span>$63,048.34</span>
+    ],
+    "correct": "$65,353.33"
   }
-  
 ];
